1766af617a4ce2da10c9f35eb94d34c738fa186c
Рыбальченко Дмитрий Николаевич <RybalchenkoDN@gnivc.ru>
2026-05-18 16:05:24 +0500

    add equip-states

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,24 +1,22 @@
 extends NodeState
 
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
-
-var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.last_direction
+	var lastDirection = GameInputEvents.last_direction
 
-	if direction == Vector2.UP:
+	if lastDirection == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.DOWN:
+	elif lastDirection == Vector2.DOWN:
 		animated_sprite_2d.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif lastDirection == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
-	elif direction == Vector2.RIGHT:
+	elif lastDirection == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
 	else:
 		animated_sprite_2d.play("idle_front")
@@ -27,6 +25,12 @@ func _on_physics_process(_delta : float) -> void:
 func _on_next_transitions() -> void:
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
+	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+		transition.emit("Chopping")
+	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
+	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("Watering")
 
 func _on_enter() -> void:
 	pass
